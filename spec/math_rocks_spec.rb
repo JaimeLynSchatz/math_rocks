41564269c4366f764f44fb06588972c7e81e1070
Jaime Lyn Schatz <jaimelynschatz@gmail.com>
2014-05-16 11:23:18 -0700

    add pluralize test to spec

--- spec/math_rocks_spec.rb
+++ spec/math_rocks_spec.rb
@@ -5,4 +5,7 @@ describe MathRocks::Factors do
     MathRocks::Factors.gcd(2, 4).should eql(2)
   end
 
+  it "pluralizes a word"
+    MathRocks::Factors.pluralize("Quotient").should eql("Quotients")
+  end
 end
