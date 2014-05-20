06e3e1b6f56f1c0d732271bbed35a5edcb8c2293
Jaime Lyn Schatz <jaimelynschatz@gmail.com>
2014-05-19 22:07:27 -0700

    weird failure for gcd, need to investigagte

--- spec/math_rocks_spec.rb
+++ spec/math_rocks_spec.rb
@@ -5,6 +5,10 @@ describe MathRocks::Factors do
     MathRocks::Factors.gcd(2, 4).should eql(2)
   end
 
+  it "the gcd of 2 and 2 is 2" do
+    MathRocks::Factors.gcd(2, 2).should eql(2)
+  end
+
   it "pluralizes a word" do
     MathRocks::Factors.pluralize("Quotient").should eql("Quotients")
   end
